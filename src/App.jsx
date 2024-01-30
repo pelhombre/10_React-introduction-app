9fc0fd3b8dbfd1c76230e255ffe421face63e312
pelhombre <elhombepol120@gmail.com>
2024-01-30 18:40:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+const name = "Hello Pol";
+
+function App() {
+  const [] = useState(0);
+
+  return (
+    <>
+      <h1>{name}</h1>
+    </>
+  );
+}
+
+export default App;
+
